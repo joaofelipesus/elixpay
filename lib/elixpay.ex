5c4e53cfdd605cd9534334bf5d070528078c1b32
joaofelipe <joaofelipe1294@gmail.com>
2022-10-22 21:08:46 -0300

    Initial commit

---
+++ lib/elixpay.ex
@@ -0,0 +1,18 @@
+defmodule Elixpay do
+  @moduledoc """
+  Documentation for `Elixpay`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixpay.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixpay_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixpayTest do
+  use ExUnit.Case
+  doctest Elixpay
+
+  test "greets the world" do
+    assert Elixpay.hello() == :world
+  end
+end
